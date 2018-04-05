6ebb626bc793e696c2e261b34d26a125c3bd1942
timetology <timetology@gmail.com>
2018-04-05 17:21:37 -0400

    Update rmt_2_renamer.py
    # .3 05April2018
    #	Added -r recursive directory traversal

--- scripts/rmt/2_Renamer/rmt_2_renamer.py
+++ scripts/rmt/2_Renamer/rmt_2_renamer.py
@@ -14,10 +14,12 @@
 #
 # e.g.: SYSTEM_de5a16da1de6a30fa691170aac750915db96cfac0cb1fa9ef83aaedfba77f945_38271nm_ -> SYSTEM_MachineName_de5a16da1de6a30fa691170aac750915db96cfac0cb1fa9ef83aaedfba77f945_38271nm_
 # Change Log
-# .1 3May2016 
+# .1 03May2016 
 #	Initial Development
-# .2 218Jul2017
+# .2 18Jul2017
 #   Added in Getpass Support
+# .3 05April2018
+#	Added -r recursive directory traversal
 
 import argparse
 import os
@@ -53,36 +55,14 @@ def main():
 	parser.add_argument('-p','--pass', dest='passwd', help='Password for SQL Database. Default: Windows Credentials', metavar='<password>')
 	parser.add_argument('-s','--server', help='Hostname or IP for SQL Server. Default: localhost', metavar='<hostname or IP>', default='LOCALHOST')
 	parser.add_argument('-db','--database', help='ECAT database', metavar='<database>', default='ECAT$PRIMARY')
+	parser.add_argument('-r','--recursive', help='Recursively traverse directory', action='store_true', default=False)
 	#parser.add_argument('-c','--copy', action='store_true', default=False, help='COPY files instead of rename (Requires -o <output directory> and it recommended you use -pre <prefix> or -suf <suffix>')
 	#parser.add_argument('-o','--output', help='Output Directory used when -c (copy) is specified', metavar='<output_directory>')
 	#parser.add_argument('-pre','--prefix', help='Prefix for files to rename or copy', metavar='<prefix>')
 	#parser.add_argument('-suf','--suffix', help='Suffix for files to rename or copy', metavar='<suffix>')
 	args = parser.parse_args()
-
-	#Check if directory is exists
-	if not (os.path.isdir(args.dir)):
-		parser.error('Directory does not exist.')
-
-	#Check if args.dir ends with a / or \
-	if not (args.dir.endswith('\\') or args.dir.endswith('/')):
-		#Append the appropriate slash
-		#Check abs path to see if windows or linux dir structure
-		print(os.path.abspath(args.dir))
-		if '\\' in str(os.path.abspath(args.dir)):
-			#path = str(os.path.abspath(args.dir) + '\\'
-			args.dir = args.dir + '\\'
-		elif '/' in str(os.path.abspath(args.dir)):
-			#path = str(os.path.abspath(args.dir) + '\'
-			args.dir = args.dir + '/'
-
-	#If -c is set, check if -o is also set and vice versa
-	#if ((args.copy and args.output is None) or (args.copy is None and args.output)):
-	#	parser.error("-c (--copy) requires -o (--output) <output_directory>")
-
-	#Check if both -pre and -suf are set as they are mutually exclusive
-	#if (args.prefix and args.suffix):
-	#	parser.error("-pre (--prefix) <prefix> and -suf (--suffix) <suffix> are mutally exclusive")
-
+	
+	#DB CONNECTION
 	#Check if only user is set and prompt for password
 	if ((args.user) and (args.passwd is None)):
 		import getpass
@@ -92,8 +72,6 @@ def main():
 	#Check if password is set
 	if ((args.user is None) and (args.passwd)):
 		parser.error("-p Password specified but -u User not assigned")
-
-
 
 	#Build query to connect to DB
 	if args.user and args.passwd:
@@ -106,28 +84,68 @@ def main():
 		db = pyodbc.connect(conn)
 	except pyodbc.Error as err:
 		parser.error(err)
+			
+	#Recursively traverse directories or just look in single dir if -r not specified
+	if (args.recursive):
+		#Recursive directory
+		dirlist = []
+		for dirpath, dirnames, files in os.walk(args.dir):
+		  for dirname in dirnames:
+			dirlist.append(os.path.join(dirpath,dirname))
 
-	#Lookup MachineName for each file in specificed Directory
-	for file in os.listdir(args.dir):
-		try:
-			MachineName = SQL_Lookup_MachineName(db,file)
-		except Exception as e:
-			print('[-] Error looking up {} {}'.format(file,e))
-		else:
-			#Insert Systemname into Filename
-			newfile = file.split('_')[0] + '_' + MachineName + '_'
-			newfile_list = file.split('_')
-			newfile_list.pop(0)	#Remove first entry we already used that
-			for item in newfile_list:
-				newfile = newfile + item + '_'
+	else:
+		dirlist = []
+		dirlist.append(args.dir)
+		
+		
+	for dir in dirlist:
+		#Check if directory is exists
+		if not (os.path.isdir(dir)):
+			parser.error('Directory does not exist.')
 
-			#Rename the file
+		#Check if dir ends with a / or \
+		if not (dir.endswith('\\') or dir.endswith('/')):
+			#Append the appropriate slash
+			#Check abs path to see if windows or linux dir structure
+			print(os.path.abspath(dir))
+			if '\\' in str(os.path.abspath(dir)):
+				#path = str(os.path.abspath(dir) + '\\'
+				dir = dir + '\\'
+			elif '/' in str(os.path.abspath(dir)):
+				#path = str(os.path.abspath(dir) + '\'
+				dir = dir + '/'
+
+		#If -c is set, check if -o is also set and vice versa
+		#if ((args.copy and args.output is None) or (args.copy is None and args.output)):
+		#	parser.error("-c (--copy) requires -o (--output) <output_directory>")
+
+		#Check if both -pre and -suf are set as they are mutually exclusive
+		#if (args.prefix and args.suffix):
+		#	parser.error("-pre (--prefix) <prefix> and -suf (--suffix) <suffix> are mutally exclusive")
+
+
+
+		#Lookup MachineName for each file in specificed Directory
+		for file in os.listdir(dir):
 			try:
-				os.rename(args.dir + file, args.dir + newfile)
-			except OSError as err:
-				print('[-] Error Renaming {} to {}...'.format(args.dir + file,newfile))
+				MachineName = SQL_Lookup_MachineName(db,file)
+			except Exception as e:
+				print('[-] Error looking up {} {}'.format(file,e))
 			else:
-				print('[+] Renamed {} to {}...'.format(args.dir + file,newfile))
+				#Insert Systemname into Filename
+				newfile = file.split('_')[0] + '_' + MachineName + '_'
+				newfile_list = file.split('_')
+				newfile_list.pop(0)	#Remove first entry we already used that
+				for item in newfile_list:
+					newfile = newfile + item + '_'
+
+				#Rename the file
+				try:
+					os.rename(dir + file, dir + newfile)
+				except OSError as err:
+					print('[-] Error Renaming {} to {}...'.format(dir + file,newfile))
+				else:
+					print('[+] Renamed {} to {}...'.format(dir + file,newfile))
 	#Close the DB
 	db.close()
 
