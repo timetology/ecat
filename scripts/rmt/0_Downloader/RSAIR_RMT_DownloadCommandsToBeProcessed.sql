bc398e535d9995d1d769d34594a9b2ddf4a2edb0
timetology <timetology@gmail.com>
2020-05-03 11:46:31 -0400

    Update and rename RSAIR_RMT_Windows_MachineCommandsToBeProcessed.sql to RSAIR_RMT_DownloadCommandsToBeProcessed.sql

--- scripts/rmt/0_Downloader/RSAIR_RMT_DownloadCommandsToBeProcessed.sql
+++ scripts/rmt/0_Downloader/RSAIR_RMT_DownloadCommandsToBeProcessed.sql
@@ -1,3 +1,15 @@
+--Summary of RMT download commands to be processed
+select mc.comment as 'Summary of RMT download commands to be processed', count(*) as 'count'
+FROM [dbo].[MachineCommands] AS mc
+WHERE
+mc.Comment LIKE 'Download RSA rmt_0_downloader_%'
+AND mc.Type = 256  
+AND mc.Processed = 0
+group by mc.comment
+order by 'count' desc
+
+--Detail of commands to be processes
+/*   
 SELECT
 mn.machinename
 ,mc.Processed
@@ -20,3 +32,4 @@ WHERE
 mc.Comment LIKE 'Download RSA rmt_0_downloader_windows %'
 AND mc.Type = 256  
 AND mc.Processed = 0
+*/
