f090786639e3492cfb5733b5b03150699f55a5af
timetology <timetology@gmail.com>
2020-05-03 15:06:16 -0400

    Create RSAIR_RMT_DownloadCommandsSummary.sql

---
+++ scripts/rmt/0_Downloader/RSAIR_RMT_DownloadCommandsSummary.sql
@@ -0,0 +1,69 @@
+--Commands to be processed by Analyst Username
+select mc.UserName as 'Commands to be processed by Analyst Username', count(*) as 'count'
+FROM [dbo].[MachineCommands] AS mc
+WHERE
+   --mc.Comment LIKE 'Download RSA rmt_0_downloader_%'
+   mc.Comment LIKE '%RSA%'
+	AND mc.Type = 256  
+	AND mc.Processed = 0
+   group by mc.Username
+   order by 'count' desc
+
+--Commands to be processed by Comment
+select mc.comment as 'Summary of RMT download commands to be processed', count(*) as 'count'
+FROM [dbo].[MachineCommands] AS mc
+WHERE
+   --mc.Comment LIKE 'Download RSA rmt_0_downloader_%'
+   mc.Comment LIKE '%RSA%'
+	AND mc.Type = 256  
+	AND mc.Processed = 0
+   group by mc.comment
+   order by 'count' desc
+
+
+--Commands to be processed by Machine Name
+select mn.machinename as 'Commands to be processed by Machine Name', count(*) as 'count'
+FROM [dbo].[MachineCommands] AS mc
+LEFT JOIN [dbo].[Machines] AS [mn] WITH(NOLOCK) ON [mn].[PK_Machines] = [mc].[FK_Machines]
+WHERE
+   --mc.Comment LIKE 'Download RSA rmt_0_downloader_%'
+   mc.Comment LIKE '%RSA%'
+	AND mc.Type = 256  
+	AND mc.Processed = 0
+   group by mn.machinename
+   order by 'count' desc
+
+--Summary of RMT download commands processed
+select mc.comment as 'Summary of RMT download commands processed', count(*) as 'count'
+FROM [dbo].[MachineCommands] AS mc
+WHERE
+mc.Comment LIKE '%RSA%'
+AND mc.Type = 256  
+AND mc.Processed <> 0
+group by mc.comment
+order by 'count' desc
+
+/*
+select 
+mn.machinename
+,mc.Processed
+,mc.CreateUTCTime
+,mc.ProcessUTCTime
+,mc.Comment
+,mc.IsAutomatic
+,mc.RetrieveCount
+,mc.UserName
+,mc.WorkStation
+,mc.RetrieveUTCTime
+,mc.Canceled
+,mc.CancelUserName
+,mc.ErrorMessage
+,mc.ErrorCode
+,mc.MachineCommandError
+from uvw_MachineCommands as mc
+LEFT JOIN [dbo].[Machines] AS [mn] WITH(NOLOCK) ON [mn].[PK_Machines] = [mc].[FK_Machines]
+WHERE
+mc.Comment LIKE 'Download RSA rmt_0_downloader_windows %'
+AND mc.Type = 256  
+AND mc.Processed = 0
+*/
