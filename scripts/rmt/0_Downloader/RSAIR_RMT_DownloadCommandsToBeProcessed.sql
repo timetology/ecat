17c83987116c662f2e79281843294006bd618d52
timetology <timetology@gmail.com>
2020-05-03 15:05:28 -0400

    Update RSAIR_RMT_DownloadCommandsToBeProcessed.sql

--- scripts/rmt/0_Downloader/RSAIR_RMT_DownloadCommandsToBeProcessed.sql
+++ scripts/rmt/0_Downloader/RSAIR_RMT_DownloadCommandsToBeProcessed.sql
@@ -1,16 +1,41 @@
---Summary of RMT download commands to be processed
+--Commands to be processed by Analyst Username
+select mc.UserName as 'Commands to be processed by Analyst Username', count(*) as 'count'
+FROM [dbo].[MachineCommands] AS mc
+WHERE
+   --mc.Comment LIKE 'Download RSA rmt_0_downloader_%'
+   mc.Comment LIKE '%RSA%'
+	AND mc.Type = 256  
+	AND mc.Processed = 0
+   group by mc.Username
+   order by 'count' desc
+
+--Commands to be processed by Comment
 select mc.comment as 'Summary of RMT download commands to be processed', count(*) as 'count'
 FROM [dbo].[MachineCommands] AS mc
 WHERE
-mc.Comment LIKE 'Download RSA rmt_0_downloader_%'
-AND mc.Type = 256  
-AND mc.Processed = 0
-group by mc.comment
-order by 'count' desc
+   --mc.Comment LIKE 'Download RSA rmt_0_downloader_%'
+   mc.Comment LIKE '%RSA%'
+	AND mc.Type = 256  
+	AND mc.Processed = 0
+   group by mc.comment
+   order by 'count' desc
 
---Detail of commands to be processed
-/*   
-SELECT
+
+--Commands to be processed by Machine Name
+select mn.machinename as 'Commands to be processed by Machine Name', count(*) as 'count'
+FROM [dbo].[MachineCommands] AS mc
+LEFT JOIN [dbo].[Machines] AS [mn] WITH(NOLOCK) ON [mn].[PK_Machines] = [mc].[FK_Machines]
+WHERE
+   --mc.Comment LIKE 'Download RSA rmt_0_downloader_%'
+   mc.Comment LIKE '%RSA%'
+	AND mc.Type = 256  
+	AND mc.Processed = 0
+   group by mn.machinename
+   order by 'count' desc
+
+
+/*
+select 
 mn.machinename
 ,mc.Processed
 ,mc.CreateUTCTime
