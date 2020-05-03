b12e7f685e408f5ba0abfa5040a64f36e30b596b
timetology <timetology@gmail.com>
2020-05-03 11:47:36 -0400

    Create RSAIR_RMT_DownloadCommandsProcessed.sql

---
+++ scripts/rmt/0_Downloader/RSAIR_RMT_DownloadCommandsProcessed.sql
@@ -0,0 +1,35 @@
+--Summary of RMT download commands processed
+select mc.comment as 'Summary of RMT download commands to be processed', count(*) as 'count'
+FROM [dbo].[MachineCommands] AS mc
+WHERE
+mc.Comment LIKE 'Download RSA rmt_0_downloader_%'
+AND mc.Type = 256  
+AND mc.Processed <> 0
+group by mc.comment
+order by 'count' desc
+
+--Detail of commands processed
+/*   
+SELECT
+mn.machinename
+,mc.Processed
+,mc.CreateUTCTime
+,mc.ProcessUTCTime
+,mc.Comment
+,mc.IsAutomatic
+,mc.RetrieveCount
+,mc.UserName
+,mc.WorkStation
+,mc.RetrieveUTCTime
+,mc.Canceled
+,mc.CancelUserName
+,mc.ErrorMessage
+,mc.ErrorCode
+,mc.MachineCommandError
+from uvw_MachineCommands as mc
+LEFT JOIN [dbo].[Machines] AS [mn] WITH(NOLOCK) ON [mn].[PK_Machines] = [mc].[FK_Machines]
+WHERE
+mc.Comment LIKE 'Download RSA rmt_0_downloader_windows %'
+AND mc.Type = 256  
+AND mc.Processed <> 0
+*/
